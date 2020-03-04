ec52776286c9582396e45a8188ee0a912653e548
ahmed <ahmed.elghoul@infor.com>
2020-03-04 16:58:59 +0100

    add users components

---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: { id: number, name: string };
+  paramSubscription:Subscription;
+  constructor(private route: ActivatedRoute) { }
+  ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+    this.paramSubscription=this.route.params
+      .subscribe(
+        (param: Params) => {
+          this.user.id = param['id'];
+          this.user.name = param['name'];
+      });
+  }
+  ngOnDestroy(){
+    this.paramSubscription.unsubscribe();
+  }
+}
+
+
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users = [
+    {
+      id: 1,
+      name: 'Smith'
+    },
+    {
+      id: 2,
+      name: 'Anna'
+    },
+    {
+      id: 3,
+      name: 'Chris'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
